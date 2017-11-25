870a15b5025137ae8b52314d51fb3add1c14b668
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-11-25 13:32:38 +1100

    Finding images in file

--- bike_blog_download/get_page_content.py
+++ bike_blog_download/get_page_content.py
@@ -2,4 +2,20 @@ with open('url_list.txt','r') as f:
 	url_list = f.readlines()
 	url_list = [i.rstrip('\n') for i in url_list]
 
-print(url_list)
+def get_page_content(url):
+	import requests
+
+	r = requests.get(url)
+	html = r.text
+
+	web_lines = html.split('\n')
+	# print(web_lines)
+
+	for i in web_lines:
+		if 'click here' in i.lower():
+			print(i)
+
+print('URL list loaded.')
+get_page_content(url_list[0])
+
+# print(url_list[0])
