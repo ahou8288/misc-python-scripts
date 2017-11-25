7cc77f0336e5862fc442600d4a15ea9a6f70c94a
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-11-25 14:09:36 +1100

    Added files from the first page

--- bike_blog_download/get_page_content.py
+++ bike_blog_download/get_page_content.py
@@ -14,15 +14,16 @@ def get_image_content(image_url):
 
 def get_image_list(web_lines):
     output = []
-    for j,i in enumerate(web_lines):
+    for j, i in enumerate(web_lines):
         if 'click here' in i.lower() and 'src' in i.lower():
             url_end_index = i.find('"  WIDTH')
             # image source is probs valid
             if url_end_index > 0 and len(i) > 10:
                 image_url = base_webpage + i[image_start_ind:url_end_index]
-                image_caption = web_lines[j+5]
-                image_caption = image_caption.lstrip('       <B >').rstrip('</B>').replace('.','')
-                output.append((image_url,image_caption))
+                image_caption = web_lines[j + 5]
+                image_caption = image_caption.lstrip(
+                    '       <B >').rstrip('</B>').replace('.', '')
+                output.append((image_url, image_caption))
     return output
 
 
@@ -34,27 +35,32 @@ def get_page_title(html):
 
 
 def get_page_text(html):
-    for i,data in enumerate(html):
+    for i, data in enumerate(html):
         if '<DIV ALIGN="LEFT" >' in data:
-            content = html[i+1]
-            content = content.replace('<P>','\n')
+            content = html[i + 1]
+            content = content.replace('<P>', '\n')
             return content
 
 
-def write_page_info(title,body,images):
+def write_page_info(title, body, images):
     import os
     import shutil
     import requests
-    
+
     base_dir = './pages/'
     folder_path = base_dir + title + '/'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    with open(folder_path + 'content.txt','w') as content_file:
+    with open(folder_path + 'content.txt', 'w') as content_file:
         content_file.write(body)
 
-    for image_url, image_caption in images:
+    print('Downloading {} images.'.format(len(images)))
 
+    for image_url, image_caption in images:
+        response = requests.get(image_url, stream=True)
+        with open(folder_path + image_caption + '.jpg', 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
 
     print('"{}" page written to file.'.format(title))
 
@@ -62,24 +68,28 @@ def write_page_info(title,body,images):
 def get_page_content(page_number, url):
     import requests
 
-    #Get the data
+    # Get the data
     r = requests.get(url)
     html = r.text
 
-    #Process the data into lines
+    # Process the data into lines
     web_lines = html.split('\n')
 
-    #Process the page into images, title and body text
+    # Process the page into images, title and body text
     page_title = get_page_title(web_lines)
 
     image_list = get_image_list(web_lines)
 
-    print(image_list)
-
     page_body_text = get_page_text(web_lines)
 
-    # write_page_info(page_title,page_body_text,image_list)
+    write_page_info(page_title,page_body_text,image_list)
 
 print('URL list loaded.')
 get_page_content(0, url_list[0])
 
+from multiprocessing import Pool
+import tqdm
+
+pool = Pool(processes=8)
+for _ in tqdm.tqdm(pool.imap_unordered(get_page_content, enumerate(url_list)), total=len(content), ncols = 160, unit='page'):
+    pass
